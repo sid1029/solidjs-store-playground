97ab3f1914ae591a218d72573fd958add3327bfb
sid1029 <sid1029@gmail.com>
2025-02-18 07:22:40 +0000

    Use '/solidjs-store-playground' as the base href in production for Github Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
-export default defineConfig({
-	build: {
-		target: 'esnext',
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					'faker-js': ['@faker-js/faker/locale/en_US']
+export default defineConfig(({ mode }) => {
+	return {
+		build: {
+			target: 'esnext',
+			rollupOptions: {
+				output: {
+					manualChunks: {
+						'faker-js': ['@faker-js/faker/locale/en_US']
+					},
+				},
+			}
+		},
+		appType: 'spa',
+		base: mode === 'production' ? '/solidjs-store-playground/' : '/',
+		resolve: {
+			alias: {
+				'@': '/src',
+			},
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@import "@/styles/variables";',
 				},
 			},
-		}
-	},
-	resolve: {
-		alias: {
-			'@': '/src',
 		},
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData: '@import "@/styles/variables";',
-			},
-		},
-	},
-	plugins: [solid()],
+		plugins: [solid()]
+	};
 });
