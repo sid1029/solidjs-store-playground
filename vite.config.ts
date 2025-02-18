7c488aa873c53ad2cea775f19bb2ec8e5a3e4e07
sid1029 <sid1029@gmail.com>
2025-02-18 07:40:42 +0000

    Cleanup vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,30 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
-export default defineConfig(({ mode }) => {
-	return {
-		build: {
-			target: 'esnext',
-			rollupOptions: {
-				output: {
-					manualChunks: {
-						'faker-js': ['@faker-js/faker/locale/en_US']
-					},
-				},
-			}
-		},
-		appType: 'spa',
-		base: mode === 'production' ? '/solidjs-store-playground' : '/',
-		resolve: {
-			alias: {
-				'@': '/src',
-			},
-		},
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: '@import "@/styles/variables";',
+export default defineConfig(({ mode }) => ({
+	appType: 'spa',
+	base: mode === 'production' ? '/solidjs-store-playground' : '/',
+	build: {
+		target: 'esnext',
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					'faker-js': ['@faker-js/faker/locale/en_US'],
 				},
 			},
 		},
-		plugins: [solid()]
-	};
-});
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@import "@/styles/variables";',
+			},
+		},
+	},
+	plugins: [solid()],
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
+}));
